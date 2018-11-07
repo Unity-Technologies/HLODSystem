a157252efdf64a3114ed2ce64197f62448dc3e71
Jangkyu Seo <jangkyu@unity3d.com>
2018-11-07 15:00:01 +0900

    Bake CombinedMesh which simply merged.

---
+++ Editor/HLODEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Unity.HLODSystem
+{
+    [CustomEditor(typeof(HLOD))]
+    public class HLODEditor : Editor
+    {
+      
+
+        void OnEnable()
+        {
+           
+        }
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            
+            if (GUILayout.Button("Generate"))
+            {
+                MaterialPreservingBatcher batcher = new MaterialPreservingBatcher();
+                batcher.Batch(((HLOD) target).HighRoot);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+}
