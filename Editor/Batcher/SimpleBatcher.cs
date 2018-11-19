953e233f422b8a0bea229c6e08e5ef3a9634c008
Jangkyu Seo <jangkyu@unity3d.com>
2018-11-19 15:46:23 +0900

    Added simple batcher options.

--- Editor/Batcher/SimpleBatcher.cs
+++ Editor/Batcher/SimpleBatcher.cs
@@ -1,10 +1,42 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Unity.HLODSystem
 {
     public class SimpleBatcher : IBatcher
     {
+
+        static class Styles
+        {
+            public static int[] PackTextureSizes = new int[]
+            {
+                256, 512, 1024, 2048, 4096
+            };
+            public static string[] PackTextureSizeNames;
+
+            public static int[] LimitTextureSizes = new int[]
+            {
+                32, 64, 128, 256, 512, 1024
+            };
+            public static string[] LimitTextureSizeNames;
+
+
+            static Styles()
+            {
+                PackTextureSizeNames = new string[PackTextureSizes.Length];
+                for (int i = 0; i < PackTextureSizes.Length; ++i)
+                {
+                    PackTextureSizeNames[i] = PackTextureSizes[i].ToString();
+                }
+
+                LimitTextureSizeNames = new string[LimitTextureSizes.Length];
+                for (int i = 0; i < LimitTextureSizes.Length; ++i)
+                {
+                    LimitTextureSizeNames[i] = LimitTextureSizes[i].ToString();
+                }
+            }
+        }
         [InitializeOnLoadMethod]
         static void RegisterType()
         {
@@ -25,7 +57,10 @@ namespace Unity.HLODSystem
                 batcherOptions.LimitTextureSize = 128;
             if (batcherOptions.Material == null)
                 batcherOptions.Material = (Material) null;
- 
+
+            batcherOptions.PackTextureSize = EditorGUILayout.IntPopup("Pack texture size", batcherOptions.PackTextureSize, Styles.PackTextureSizeNames, Styles.PackTextureSizes);
+            batcherOptions.LimitTextureSize = EditorGUILayout.IntPopup("Limit texture size", batcherOptions.LimitTextureSize, Styles.LimitTextureSizeNames, Styles.LimitTextureSizes);
+            
             batcherOptions.Material = EditorGUILayout.ObjectField("Material", batcherOptions.Material, typeof(Material), false) as Material;
             EditorGUI.indentLevel -= 1;
         }
