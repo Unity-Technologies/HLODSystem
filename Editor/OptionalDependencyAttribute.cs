11f3a1bc2c725f3b6b554190bee2538186ea4ab8
Jangkyu Seo <jangkyu@unity3d.com>
2018-11-23 11:12:05 +0900

    Added OptionalDependencyAttribute which auto set a define.

---
+++ Editor/OptionalDependencyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Unity.HLODSystem
+{
+    [Conditional("UNITY_CCU")]
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    class OptionalDependencyAttribute : Attribute
+    {
+        public string dependentClass;
+        public string define;
+
+        public OptionalDependencyAttribute(string dependentClass, string define)
+        {
+            this.dependentClass = dependentClass;
+            this.define = define;
+        }
+    }
+}
