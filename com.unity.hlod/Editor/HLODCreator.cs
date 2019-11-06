7e35d1866b15640bde2728c8f40de97a22090456
Jangkyu Seo <jangkyu@unity3d.com>
2019-11-06 16:16:20 +0900

    Changed not to proceed if the node does not exist.

--- com.unity.hlod/Editor/HLODCreator.cs
+++ com.unity.hlod/Editor/HLODCreator.cs
@@ -158,17 +158,13 @@ namespace Unity.HLODSystem
                 using (DisposableList<HLODBuildInfo> buildInfos = CreateBuildInfo(rootNode, hlod.MinObjectSize))
                 {
                     if (buildInfos.Count == 0)
-                        yield break;
-
-                    if (buildInfos[0].WorkingObjects.Count == 0)
                     {
-                        if (EditorUtility.DisplayDialog("Empty HLOD sources.",
-                                "There are no objects to be included in the HLOD.\nDo you want to continue anyway?",
-                                "Continue", "Cancel") == false)
-                        {
-                            yield break;
-                        }
+                        EditorUtility.DisplayDialog("Empty HLOD sources.",
+                            "There are no objects to be included in the HLOD.",
+                            "Ok");
+                        yield break;
                     }
+                  
                     
                     Debug.Log("[HLOD] Splite space: " + sw.Elapsed.ToString("g"));
                     sw.Reset();
