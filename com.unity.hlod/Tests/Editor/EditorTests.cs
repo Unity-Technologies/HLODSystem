505d078f6afe12811063bd10d7da82e17c30d1a9
Ulughbek Muslimov <bek@unity3d.com>
2019-11-29 14:06:11 +0900

    Enabled Package Testing for HLOD; Added Test Scene used for EditMode Tests

---
+++ com.unity.hlod/Tests/Editor/EditorTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Unity.HLODSystem.EditorTests
+{
+    public class EditorTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void Test_01SimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator CreatedHlodInstance()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+        
+        [UnityTest]
+        public IEnumerator DestroysHlodInstabce()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
