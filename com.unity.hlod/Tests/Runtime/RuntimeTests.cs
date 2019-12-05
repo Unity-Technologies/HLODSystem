f8deda834acdb776dd09e1f7ba4377bad419a396
Ulughbek Muslimov <bek@unity3d.com>
2019-12-05 18:43:35 +0900

    Added more PlayMode Tests

--- com.unity.hlod/Tests/Runtime/RuntimeTests.cs
+++ com.unity.hlod/Tests/Runtime/RuntimeTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using Unity.HLODSystem.Utils;
 using UnityEditor;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.TestTools;
 
@@ -12,18 +13,31 @@ namespace Unity.HLODSystem.RuntimeTests
     {
         private GameObject mGameObject;
         private GameObject mHlodGameObject;
-        
+        private GameObject mHlodCameraObject;
+
         [SetUp]
         public void SetUp()
         {
-            mGameObject = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/TestAssets/Prefabs/HLODTestPrefabBaked.prefab");
+            mGameObject =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/TestAssets/Prefabs/HLODTestPrefabBaked.prefab");
             mGameObject = GameObject.Instantiate(mGameObject, new Vector3(0, 0, 10), new Quaternion(0, 180, 0, 0));
             new WaitForSeconds(0.1f);
-            
+
             Assert.NotNull(mGameObject);
-            
-            mHlodGameObject= mGameObject.transform.Find("HLOD").gameObject ;
+
+            mHlodGameObject = mGameObject.transform.Find("HLOD").gameObject;
             Assert.NotNull(mHlodGameObject);
+
+            mHlodCameraObject = mGameObject.transform.Find("HLOD Camera").gameObject;
+            Assert.NotNull(mHlodCameraObject);
+        }
+        
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(mHlodCameraObject);
+            Object.Destroy(mHlodGameObject);
+            Object.Destroy(mGameObject);
         }
 
         [Test]
@@ -32,11 +46,53 @@ namespace Unity.HLODSystem.RuntimeTests
             int childrenCount = mHlodGameObject.transform.childCount;
             Assert.True(childrenCount == 11);
         }
-        
-        /*[UnityTest]
-        public IEnumerator BakeHlodForGameObject()
+
+        [Test]
+        public void CameraHasHlodCameraRecognizerComponent()
         {
-            yield return null;
-        }*/
+            HLODCameraRecognizer hlodCameraRecognizer = mHlodCameraObject.GetComponent<HLODCameraRecognizer>();
+            Assert.NotNull(hlodCameraRecognizer);
+        }
+
+        [Test]
+        public void MainCameraDoesNotSeeAnyHlodObjects()
+        {
+            Camera mainCamera = mGameObject.transform.Find("Main Camera").gameObject.GetComponent<Camera>();
+            Assert.NotNull(mainCamera);
+
+            foreach (Transform child in mHlodGameObject.transform)
+            {
+                Debug.Log(child.name + ": " + IsTargetVisible(mainCamera, child.gameObject));
+                //Assert.False(IsTargetVisible(mainCamera, child.gameObject));
+            }
+        }
+
+        [Test]
+        public void HlodCameraSeesHlodObjects()
+        {
+            Camera hlodCamera = mHlodCameraObject.GetComponent<Camera>();
+
+            foreach (Transform child in mHlodGameObject.transform)
+            {
+                Debug.Log(child.name + ": " + IsTargetVisible(hlodCamera, child.gameObject));
+                Assert.True(IsTargetVisible(hlodCamera, child.gameObject));
+            }
+        }
+
+        bool IsTargetVisible(Camera c, GameObject go)
+        {
+            var planes = GeometryUtility.CalculateFrustumPlanes(c);
+            var point = go.transform.position;
+
+            foreach (var plane in planes)
+            {
+                float distance = plane.GetDistanceToPoint(point);
+
+                if (distance < 0.0f)
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
