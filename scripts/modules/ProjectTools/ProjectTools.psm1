b0de41be5f1ad193b410e8163dabeb439b5815a6
Ulughbek Muslimov <bek@unity3d.com>
2019-12-12 12:59:52 +0900

    Added Testrun Scripts

---
+++ scripts/modules/ProjectTools/ProjectTools.psm1
@@ -0,0 +1,5 @@
+. $PSScriptRoot/function-Check-LastExitCode.ps1
+. $PSScriptRoot/function-Get-Configuration.ps1
+
+Export-ModuleMember Check-LastExitCode
+Export-ModuleMember Get-Configuration
---
+++ scripts/modules/ProjectTools/function-Get-Configuration.ps1
@@ -0,0 +1,31 @@
+function Get-Configuration {
+    $username = Get-Current-Username
+    $projectRootPath = Get-Project-Root-Path
+    $projectPath = "$projectRootPath/com.unity.hlod/Samples~"
+    #$editorExecutablePath = "C:/Program Files\Unity\Hub\Editor\2019.2.14f1\Editor\Unity.exe"
+    $editorExecutablePath = "C:/Users/$username/m2/M2/development/Unity/WinEditor/Unity.exe"
+    $editorLogPath = "C:/Users/$username/AppData/Local/Unity/Editor/Editor.log"
+    $editorCrashLogPath = "C:/Users/$username/AppData/Local/Temp/Unity/Editor/Crashes"    
+    $testResultsOutputFolder = "$projectRootPath/TestResults"
+    $logOutputFolder = "$projectRootPath/Logs"
+    
+    $result = @{
+        "EditorExecutablePath"    = $editorExecutablePath;
+        "EditorLogPath"           = $editorLogPath;
+        "EditorCrashLogPath"      = $editorCrashLogPath;
+        "ProjectRootPath"         = $projectRootPath;
+        "ProjectPath"             = $projectPath;
+        "TestResultsOutputFolder" = $testResultsOutputFolder;
+        "LogOutputFolder"         = $logOutputFolder;
+    }
+
+    return $result;
+}
+
+function Get-Current-Username {
+    return $env:username
+}
+
+function Get-Project-Root-Path {
+    return  Get-Location | Split-Path -Parent
+}
