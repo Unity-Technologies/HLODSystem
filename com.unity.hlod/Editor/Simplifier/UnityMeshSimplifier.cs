e7d1b065b919b54e6a9d75df9c3d6ae7a9d9bd9c
Jangkyu Seo <jangkyu@unity3d.com>
2020-02-11 09:54:00 +0900

    Make public and make set to callable from outside of project.

--- com.unity.hlod/Editor/Simplifier/UnityMeshSimplifier.cs
+++ com.unity.hlod/Editor/Simplifier/UnityMeshSimplifier.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 
 namespace Unity.HLODSystem.Simplifier
 {
-    class UnityMeshSimplifier : SimplifierBase
+    public class UnityMeshSimplifier : SimplifierBase
     {
 
         [InitializeOnLoadMethod]
