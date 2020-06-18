14436e5fee672ecfbb682dce071a5796fc2c9f70
jangkyu <sjk0125@gmail.com>
2020-06-18 18:26:07 +0900

    reorgnazed sample resources.

---
+++ com.unity.hlod.addressable/Samples~/Assets/Tests/EditMode/EditorTests.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using Unity.HLODSystem.Streaming;
+using Unity.HLODSystem.Utils;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+using UnityEngine.AddressableAssets;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine.ResourceManagement.ResourceLocations;
+
+namespace Unity.HLODSystem.EditorTests
+{
+    [TestFixture]
+    public class EditorTests
+    {
+        private const string m_prefabName = "Assets/TestAssets/Prefabs/HLODTestPrefab.prefab";
+        private HLOD m_hlod;
+        private GameObject m_hlodGameObject;
+        private int childrenCount;
+
+        [SetUp]
+        public void Setup()
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(m_prefabName);
+            m_hlodGameObject = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+            PrefabUtility.UnpackPrefabInstance(m_hlodGameObject, PrefabUnpackMode.OutermostRoot, InteractionMode.AutomatedAction);
+            m_hlod = m_hlodGameObject.GetComponentInChildren<HLOD>();
+            var coroutine = CoroutineRunner.RunCoroutine(HLODCreator.Create(m_hlod));
+
+            while (coroutine.MoveNext())
+            {
+                //Wait until coroutine is finished
+            }
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            var coroutine = CoroutineRunner.RunCoroutine(HLODCreator.Destroy(m_hlod));
+
+            while(coroutine.MoveNext())
+            {
+
+            }
+        }
+
+        [Test]
+        public void ComponentTest()
+        {
+            Assert.NotNull(m_hlodGameObject);
+            Assert.NotNull(m_hlod);
+            Assert.NotNull(m_hlod.GetComponent<AddressableHLODController>());
+        }
+
+        [Test]
+        public void AddressableHLODControllerTest()
+        {
+            string[] lowAddrs =
+            {
+                "Assets/TestAssets/Artifacts/HLOD_group-1.hlod[]",
+                "Assets/TestAssets/Artifacts/HLOD_group0.hlod[_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group0.hlod[_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group0.hlod[_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group0.hlod[_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group1.hlod[_1_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group1.hlod[_1_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group1.hlod[_1_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group1.hlod[_1_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group2.hlod[_2_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group2.hlod[_2_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group2.hlod[_2_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group2.hlod[_2_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group3.hlod[_3_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group3.hlod[_3_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group3.hlod[_3_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group3.hlod[_3_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group4.hlod[_4_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group4.hlod[_4_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group4.hlod[_4_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group4.hlod[_4_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group5.hlod[_1_1_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group5.hlod[_1_1_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group5.hlod[_1_1_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group5.hlod[_1_1_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group6.hlod[_1_2_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group6.hlod[_1_2_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group6.hlod[_1_2_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group6.hlod[_1_2_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group7.hlod[_1_3_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group7.hlod[_1_3_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group7.hlod[_1_3_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group7.hlod[_1_3_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group8.hlod[_1_4_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group8.hlod[_1_4_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group8.hlod[_1_4_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group8.hlod[_1_4_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group9.hlod[_2_1_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group9.hlod[_2_1_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group9.hlod[_2_1_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group9.hlod[_2_1_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group10.hlod[_2_2_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group10.hlod[_2_2_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group10.hlod[_2_2_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group10.hlod[_2_2_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group11.hlod[_2_3_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group11.hlod[_2_3_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group11.hlod[_2_3_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group11.hlod[_2_3_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group12.hlod[_2_4_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group12.hlod[_2_4_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group12.hlod[_2_4_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group12.hlod[_2_4_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group13.hlod[_3_1_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group13.hlod[_3_1_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group13.hlod[_3_1_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group13.hlod[_3_1_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group14.hlod[_3_2_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group14.hlod[_3_2_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group14.hlod[_3_2_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group14.hlod[_3_2_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group15.hlod[_3_3_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group15.hlod[_3_3_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group15.hlod[_3_3_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group15.hlod[_3_3_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group16.hlod[_3_4_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group16.hlod[_3_4_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group16.hlod[_3_4_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group16.hlod[_3_4_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group17.hlod[_4_1_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group17.hlod[_4_1_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group17.hlod[_4_1_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group17.hlod[_4_1_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group18.hlod[_4_2_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group18.hlod[_4_2_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group18.hlod[_4_2_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group18.hlod[_4_2_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group19.hlod[_4_3_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group19.hlod[_4_3_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group19.hlod[_4_3_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group19.hlod[_4_3_4]",
+                "Assets/TestAssets/Artifacts/HLOD_group20.hlod[_4_4_1]",
+                "Assets/TestAssets/Artifacts/HLOD_group20.hlod[_4_4_2]",
+                "Assets/TestAssets/Artifacts/HLOD_group20.hlod[_4_4_3]",
+                "Assets/TestAssets/Artifacts/HLOD_group20.hlod[_4_4_4]"
+            };
+
+            var controller = m_hlod.GetComponent<AddressableHLODController>();
+            Assert.AreEqual(84, controller.Container.Count);
+            Assert.AreEqual(100, controller.HighObjectCount);
+            Assert.AreEqual(85, controller.LowObjectCount);
+
+            for ( int i = 0; i < 85; ++i )
+            {
+                Assert.AreEqual(lowAddrs[i], controller.GetLowObjectAddr(i));
+            }
+        }
+
+        [Test]
+        public void AddressableTest()
+        {
+            var controller = m_hlod.GetComponent<AddressableHLODController>();
+            var settings = AddressableAssetSettingsDefaultObject.Settings;
+
+            Assert.NotNull(settings);
+            Assert.AreEqual(85, controller.LowObjectCount);
+
+            var group = settings.FindGroup("HLOD");
+            Assert.NotNull(group);
+
+            List<AddressableAssetEntry> entries = new List<AddressableAssetEntry>();
+            group.GatherAllAssets(entries, true, false, false);
+            Assert.AreEqual(22, entries.Count);
+
+            List<AddressableAssetEntry> allEntries = new List<AddressableAssetEntry>();
+            group.GatherAllAssets(allEntries, false, false, true);
+            for ( int i = 0; i < 85; ++i )
+            {
+                Assert.IsTrue(AddressableResourceExists(controller.GetLowObjectAddr(i), allEntries));
+            }
+
+            
+
+        }
+
+        private static bool AddressableResourceExists(string key, List<AddressableAssetEntry> assetEntries)
+        {
+            for (int i = 0; i < assetEntries.Count; ++i )
+            {
+                if (assetEntries[i].address == key)
+                    return true;
+            }
+            
+            return false;
+        }
+
+
+
+    }
+}
---
+++ com.unity.hlod.addressable/Samples~/Assets/Tests/PlayMode/RuntimeTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using Unity.HLODSystem.Streaming;
+using Unity.HLODSystem.Utils;
+using UnityEditor;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Object = UnityEngine.Object;
+
+namespace Unity.HLODSystem.RuntimeTests
+{
+    [TestFixture]
+    public class RuntimeTests : IPrebuildSetup, IPostBuildCleanup
+    {
+        private GameObject mGameObject;
+        private GameObject mHlodGameObject;
+        private GameObject mHlodCameraObject;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            mGameObject =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/TestAssets/Prefabs/HLODTestPrefabBaked.prefab");
+            mGameObject = GameObject.Instantiate(mGameObject, Vector3.zero, Quaternion.identity);
+            Assert.NotNull(mGameObject);
+
+            mHlodGameObject = mGameObject.transform.Find("HLOD").gameObject;
+            Assert.NotNull(mHlodGameObject);
+
+            mHlodGameObject.GetComponentInChildren<HLODControllerBase>().Install();
+
+            mHlodCameraObject = mGameObject.transform.Find("Main Camera").gameObject;
+            Assert.NotNull(mHlodCameraObject);
+
+            UnityEditor.AddressableAssets.Settings.AddressableAssetSettings.BuildPlayerContent();
+        }
+
+        public void Cleanup()
+        {
+            Object.Destroy(mHlodCameraObject);
+            Object.Destroy(mHlodGameObject);
+            Object.Destroy(mGameObject);
+        }
+
+        [Test]
+        public void HlodGameObjectHasChildren()
+        {
+            int childrenCount = mHlodGameObject.transform.childCount;
+            Assert.True(childrenCount == 11);
+        }
+
+        [Test]
+        public void CameraHasHlodCameraRecognizerComponent()
+        {
+            HLODCameraRecognizer hlodCameraRecognizer = mHlodCameraObject.GetComponent<HLODCameraRecognizer>();
+            Assert.NotNull(hlodCameraRecognizer);
+        }
+
+        [UnityTest]
+        [TestCase("Assets/TestAssets/RawTestData/TestData_1.json", ExpectedResult = null)]
+        [TestCase("Assets/TestAssets/RawTestData/TestData_2.json", ExpectedResult = null)]
+        [TestCase("Assets/TestAssets/RawTestData/TestData_3.json", ExpectedResult = null)]
+        [TestCase("Assets/TestAssets/RawTestData/TestData_4.json", ExpectedResult = null)]
+        [TestCase("Assets/TestAssets/RawTestData/TestData_5.json", ExpectedResult = null)]
+        public IEnumerator HlodKickedForSpecificObjects(string testDataPath)
+        {
+            TestData testData = TestData.CreateFromJson(testDataPath);
+
+            SetUpCamera(testData.cameraSettings);
+
+            var cam = mHlodCameraObject.GetComponent<Camera>();
+            
+            for (int i = 0; i < 3; ++i)
+            {
+                HLODManager.Instance.OnPreCull(cam);
+            }
+            
+            CheckGameObjectActiveState(testData.listOfGameObjects);
+            CheckHlodObjectsActiveState(testData.listOfActiveHlods);
+
+            yield return null;
+        }
+
+        private void SetUpCamera(CameraSettings cameraSettings)
+        {
+            Camera hlodCamera = mHlodCameraObject.GetComponent<Camera>();
+
+            hlodCamera.transform.position = new Vector3(
+                cameraSettings.location.x,
+                cameraSettings.location.y,
+                cameraSettings.location.z);
+
+            hlodCamera.transform.eulerAngles = new Vector3(
+                cameraSettings.rotation.x,
+                cameraSettings.rotation.y + 180,
+                cameraSettings.rotation.z);
+
+            HLODManager.Instance.OnPreCull(hlodCamera);
+        }
+
+        private void CheckGameObjectActiveState(List<PlayModeTestGameObject> listOfGameObjects)
+        {
+            foreach (PlayModeTestGameObject playModeTestGameObject in listOfGameObjects)
+            {
+                Transform rinNumbers = mHlodGameObject.transform.Find(playModeTestGameObject.groupName);
+
+                for (int i = 0; i < rinNumbers.childCount; i++)
+                    Assert.AreEqual(rinNumbers.GetChild(i).gameObject.activeSelf, playModeTestGameObject.enabled[i]);
+            }
+        }
+
+        private void CheckHlodObjectsActiveState(List<string> listOfActiveHlods)
+        {
+            //Just in case when this list gets too big in future TestCases
+            HashSet<string> hashSet = new HashSet<string>(listOfActiveHlods);
+
+            Transform hlods = mHlodGameObject.transform.Find("HLODRoot");
+
+            foreach (Transform child in hlods.transform)
+                Assert.AreEqual(child.gameObject.activeSelf, hashSet.Contains(child.gameObject.name));
+        }
+    }
+
+    [Serializable]
+    public class TestData
+    {
+        public CameraSettings cameraSettings;
+        public List<PlayModeTestGameObject> listOfGameObjects;
+        public List<string> listOfActiveHlods;
+
+        public static TestData CreateFromJson(string jsonFilePath)
+        {
+            if (!File.Exists(jsonFilePath))
+                throw new FileNotFoundException();
+
+            string dataAsJson = File.ReadAllText(jsonFilePath);
+
+            return JsonUtility.FromJson<TestData>(dataAsJson);
+        }
+    }
+
+    [Serializable]
+    public class CustomVector3
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    [Serializable]
+    public class PlayModeTestGameObject
+    {
+        public string groupName;
+        public bool[] enabled;
+    }
+
+    [Serializable]
+    public class CameraSettings
+    {
+        public CustomVector3 location;
+        public CustomVector3 rotation;
+    }
+}
