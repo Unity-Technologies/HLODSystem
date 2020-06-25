2f8947043044f008ac69a7967aa67067cded73a5
jangkyu <sjk0125@gmail.com>
2020-06-25 18:42:49 +0900

    add com.unity.hlod.addressable test on ci

---
+++ com.unity.hlod.addressable/Tests/Editor/EditorTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+class EditorTests
+{
+    [Test]
+    public void DummyTestToBeImplemented()
+    {
+        int a = 1;
+        int b = 1;
+
+        Assert.True(a == b);
+    }
+}
