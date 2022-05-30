965d102e1e77ea38804308ce9344698da2507b4a
Jangkyu Seo <jangkyu@unity3d.com>
2022-05-30 20:53:20 +0900

    Refactoring GetMeshRenderer

--- com.unity.hlod/Editor/CreateUtils/GetMeshRenderer.cs
+++ com.unity.hlod/Editor/CreateUtils/GetMeshRenderer.cs
@@ -7,41 +7,58 @@ namespace Unity.HLODSystem
 {
     public static partial class CreateUtils
     {
-        public static List<MeshRenderer> GetMeshRenderers(List<GameObject> gameObjects, float minObjectSize)
+        private class MeshRendererCalculator
         {
-            List<MeshRenderer> meshRenderers = new List<MeshRenderer>();
+            private bool m_isCalculated = false;
+            private List<MeshRenderer> m_meshRenderers = new List<MeshRenderer>();
+            private List<LODGroup> m_lodGroups = new List<LODGroup>();
 
-            for (int oi = 0; oi < gameObjects.Count; ++oi)
+            private List<MeshRenderer> m_resultMeshRenderers = new List<MeshRenderer>();
+
+            public List<MeshRenderer> ResultMeshRenderers
             {
-                GameObject obj = gameObjects[oi];
-
-                if (obj.activeInHierarchy == false)
-                    continue;
-
-                LODGroup[] lodGroups = obj.GetComponentsInChildren<LODGroup>();
-                List<MeshRenderer> allRenderers = obj.GetComponentsInChildren<MeshRenderer>().ToList();
-
-                for (int gi = 0; gi < lodGroups.Length; ++gi)
+                get { return m_resultMeshRenderers; }
+            }
+            
+            public MeshRendererCalculator(List<GameObject> targetGameObjects)
+            {
+                for (int oi = 0; oi < targetGameObjects.Count; ++oi)
                 {
-                    LODGroup lodGroup = lodGroups[gi];
-                    if (lodGroup.enabled == false || lodGroup.gameObject.activeInHierarchy == false)
-                        continue;
+                    var target = targetGameObjects[oi];
+                    
+                    m_meshRenderers.AddRange(target.GetComponentsInChildren<MeshRenderer>());
+                    m_lodGroups.AddRange(target.GetComponentsInChildren<LODGroup>());
+                }
 
-                    //all renderers using in LODGroup should be removed in the allRenderers
+                RemoveDisabled(m_lodGroups);
+                RemoveDisabled(m_meshRenderers);
+            }
+
+            public void Calculate(float minObjectSize)
+            {
+                if (m_isCalculated == true)
+                    return;
+                
+                for (int gi = 0; gi < m_lodGroups.Count; ++gi)
+                {
+                    LODGroup lodGroup = m_lodGroups[gi];
                     LOD[] lods = lodGroup.GetLODs();
                     for (int li = 0; li < lods.Length; ++li)
                     {
                         Renderer[] lodRenderers = lods[li].renderers;
+
+                        //Remove every mesh renderer which is registered to the LODGroup.
                         for (int ri = 0; ri < lodRenderers.Length; ++ri)
                         {
                             MeshRenderer mr = lodRenderers[ri] as MeshRenderer;
                             if (mr == null)
                                 continue;
 
-                            allRenderers.Remove(mr);
+                            m_meshRenderers.Remove(mr);
                         }
                     }
 
+                    //
                     Renderer[] renderers = lods.Last().renderers;
                     for (int ri = 0; ri < renderers.Length; ++ri)
                     {
@@ -57,28 +74,64 @@ namespace Unity.HLODSystem
                         if (max < minObjectSize)
                             continue;
 
-                        meshRenderers.Add(mr);
+                        m_resultMeshRenderers.Add(mr);
                     }
                 }
 
-                for (int ai = 0; ai < allRenderers.Count; ++ai)
+                for (int mi = 0; mi < m_meshRenderers.Count; ++mi)
                 {
-                    MeshRenderer mr = allRenderers[ai] as MeshRenderer;
-                    ;
-
-                    if (mr.enabled == false || mr.gameObject.activeInHierarchy == false)
-                        continue;
-
+                    MeshRenderer mr = m_meshRenderers[mi];
+                    
                     float max = Mathf.Max(mr.bounds.size.x, mr.bounds.size.y, mr.bounds.size.z);
                     if (max < minObjectSize)
                         continue;
 
-                    meshRenderers.Add(allRenderers[ai]);
+                    m_resultMeshRenderers.Add(mr);
                 }
+
+                m_isCalculated = true;
             }
 
-            return meshRenderers;
+            //enabled is not Component variable.
+            //So we can not make this to Generic function.
+            private void RemoveDisabled(List<LODGroup> componentList)
+            {
+                for (int i = 0; i < componentList.Count; ++i)
+                {
+                    if (componentList[i].enabled == true && componentList[i].gameObject.activeInHierarchy == true)
+                    {
+                        continue;
+                    }
+
+                    int backIndex = componentList.Count - 1;
+                    componentList[i] = componentList[backIndex];
+                    componentList.RemoveAt(backIndex);
+                    i -= 1;
+                }
+            }
+            private void RemoveDisabled(List<MeshRenderer> componentList)
+            {
+                for (int i = 0; i < componentList.Count; ++i)
+                {
+                    if (componentList[i].enabled == true && componentList[i].gameObject.activeInHierarchy == true)
+                    {
+                        continue;
+                    }
+
+                    int backIndex = componentList.Count - 1;
+                    componentList[i] = componentList[backIndex];
+                    componentList.RemoveAt(backIndex);
+                    i -= 1;
+                }
+            }
         }
+        public static List<MeshRenderer> GetMeshRenderers(List<GameObject> gameObjects, float minObjectSize)
+        {
+            MeshRendererCalculator calculator = new MeshRendererCalculator(gameObjects);
+            calculator.Calculate(minObjectSize);
+            return calculator.ResultMeshRenderers;
+        }
+
     }
 
 }
